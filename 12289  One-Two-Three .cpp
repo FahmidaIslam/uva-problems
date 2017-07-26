4514d26d2f9e79dc32cced2a1e82fe70027db731
Fahmida Islam Lima <fhamidalima20@gmail.com>
2017-07-26 18:14:51 +0600

    Add files via upload

---
+++ 12289  One-Two-Three .cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    string s;
+    cin>>t;
+    while(t--)
+    {
+        cin>>s;
+        if(s.size()>3) {cout<<'3'<<endl;}
+        else
+        {
+            if((s[0]=='t' && s[1]=='w') || (s[0]=='t' &&s[2]=='o')||(s[1]=='w' &&s[2]=='o')) {cout<<2<<endl;}
+            else {cout<<1<<endl;}
+        }
+    }
+    return 0;
+}
